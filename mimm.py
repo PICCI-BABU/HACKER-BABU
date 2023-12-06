f625f6c5594b23f309223250dd729a224fcb5d80
HACKER BABU 78 <152757328+PICCI-BABU@users.noreply.github.com>
2023-12-06 16:56:44 +0600

    Add files via upload

---
+++ mimm.py
@@ -0,0 +1,10 @@
+import sys, time
+
+def babu(z):
+  
+    for a in z +'\n':
+        sys.stdout.write(a)
+        sys.stdout.flush,()
+        time.sleep(0.04)
+        
+babu(' kire khankirpola amr tools us kore amakei pore gali des tor mare xudi.sob somoy ablami xudbina ok ')
